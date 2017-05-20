f7a1c1914f8c87ca4a5a6e28be74e1a4046fb8a9
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-05-20 14:35:19 +0100

    New AR component

---
+++ Dynamic/AttackReleaseFilter.cpp
@@ -0,0 +1,63 @@
+/**
+ * \file AttackReleaseFilter.cpp
+ */
+
+#include "AttackReleaseFilter.h"
+
+#include "../JUCE/LookAndFeel.h"
+
+namespace ATK
+{
+  namespace juce
+  {
+    AttackReleaseFilterComponent::AttackReleaseFilterComponent (::juce::AudioProcessorValueTreeState& paramState, const std::string& attackName, const std::string& releaseName)
+    : attackSlider(::juce::Slider::SliderStyle::Rotary, ::juce::Slider::TextEntryBoxPosition::TextBoxBelow), releaseSlider(::juce::Slider::SliderStyle::Rotary, ::juce::Slider::TextEntryBoxPosition::TextBoxBelow), color(::juce::Colour(56, 56, 56))
+    {
+      addAndMakeVisible(attackSlider);
+      attackAtt.reset(new ::juce::AudioProcessorValueTreeState::SliderAttachment (paramState, attackName, attackSlider));
+      attackSlider.setRange (0, 1000);
+      attackSlider.setValue(10);
+      attackSlider.setTextValueSuffix (" ms");
+      attackSlider.setColour(::juce::Slider::rotarySliderFillColourId, ::juce::Colours::rosybrown);
+      attackSlider.setLookAndFeel(&SimpleSliderLookAndFeel::get_instance());
+      
+      addAndMakeVisible(attackLabel);
+      attackLabel.setText("Attack", ::juce::NotificationType::dontSendNotification);
+      attackLabel.setJustificationType(::juce::Justification::centred);
+
+      addAndMakeVisible(releaseSlider);
+      releaseAtt.reset(new ::juce::AudioProcessorValueTreeState::SliderAttachment (paramState, releaseName, releaseSlider));
+      releaseSlider.setRange (0, 1000);
+      releaseSlider.setValue(10);
+      releaseSlider.setTextValueSuffix (" ms");
+      releaseSlider.setColour(::juce::Slider::rotarySliderFillColourId, ::juce::Colours::skyblue);
+      releaseSlider.setLookAndFeel(&SimpleSliderLookAndFeel::get_instance());
+      
+      addAndMakeVisible(releaseLabel);
+      releaseLabel.setText("Release", ::juce::NotificationType::dontSendNotification);
+      releaseLabel.setJustificationType(::juce::Justification::centred);
+
+      // Make sure that before the constructor has finished, you've set the
+      // editor's size to whatever you need it to be.
+      setSize (300, 150);
+    }
+    
+    AttackReleaseFilterComponent::~AttackReleaseFilterComponent()
+    {
+    }
+    
+    void AttackReleaseFilterComponent::paint(::juce::Graphics& g)
+    {
+      g.setColour(color);
+      g.fillRoundedRectangle(5, 5, getWidth() - 10, getHeight() - 10, 10);
+    }
+
+    void AttackReleaseFilterComponent::resized()
+    {
+      attackLabel.setBoundsRelative(0.15, 0.05, 0.2, 0.1);
+      attackSlider.setBoundsRelative(0.05, 0.2, 0.4, 0.7);
+      releaseLabel.setBoundsRelative(0.64, 0.05, 0.2, 0.1);
+      releaseSlider.setBoundsRelative(0.55, 0.2, 0.4, 0.7);
+    }
+  }
+}
---
+++ Dynamic/AttackReleaseFilter.h
@@ -0,0 +1,45 @@
+/**
+ * \file AttackReleaseFilter.h
+ */
+
+#ifndef ATKJUCECOMPONENTS_DYNAMIC_ATTACKRELEASEFILTER
+#define ATKJUCECOMPONENTS_DYNAMIC_ATTACKRELEASEFILTER
+
+#include <AppConfig.h>
+
+#include <juce_gui_basics/juce_gui_basics.h>
+#include <juce_audio_processors/juce_audio_processors.h>
+
+#include <ATK/Dynamic/AttackReleaseFilter.h>
+
+namespace ATK
+{
+  namespace juce
+  {
+    class AttackReleaseFilterComponent  : public ::juce::Component
+    {
+    public:
+      AttackReleaseFilterComponent(::juce::AudioProcessorValueTreeState& paramState, const std::string& attackName, const std::string& releaseName);
+      ~AttackReleaseFilterComponent();
+      
+      //==============================================================================
+      void paint(::juce::Graphics&) override;
+      void resized() override;
+      void set_color(::juce::Colour color);
+      
+    private:
+      ::juce::Slider attackSlider;
+      ::juce::Slider releaseSlider;
+      ::juce::Label attackLabel;
+      ::juce::Label releaseLabel;
+      ::juce::Colour color;
+      
+      std::unique_ptr<::juce::AudioProcessorValueTreeState::SliderAttachment> attackAtt;
+      std::unique_ptr<::juce::AudioProcessorValueTreeState::SliderAttachment> releaseAtt;
+
+      JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AttackReleaseFilterComponent)
+    };
+  }
+}
+
+#endif
